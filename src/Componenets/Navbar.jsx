2b61202c3aea7c41711e72e25a3b96007791a373
Jairaj Ameria <jairajameria@gmail.com>
2024-09-21 16:58:06 +0530

    adding hyperlink

--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -10,10 +10,10 @@ const Navbar = () => {
         <img className="mx-2 w-10" src={logo} alt="logo" />
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-      <FaLinkedin />
-      <FaGithub />
-      <FaSquareXTwitter />
-      <FaInstagram />
+      <a href="https://www.linkedin.com/in/jairajameria/"><FaLinkedin /></a>
+      <a href="https://github.com/jaiz-2011"><FaGithub /></a>
+      <a href="https://x.com/JairajAmeria"><FaSquareXTwitter /></a>
+      <a href="https://www.instagram.com/jaizzy2011/"><FaInstagram /></a>
     </div>
   </nav>
 }
